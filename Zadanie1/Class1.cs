24fdc8e9fd8117efc3c33a72230a664671173c3c
Grzegorz Domanowski <grzegorz.domanowski.ais@gmail.com>
2022-03-09 13:01:54 +0100

    Dodaj pliki projektów.

---
+++ Zadanie1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal interface ImaszynaLosujaca
+{
+    public void dodajNowyKupon(string value);
+
+    public List<string> wszystkieKupony();
+
+    public string wylowsowanyKupon();
+
+    public bool listaKuponowPusta();
+}
+
+namespace Zadanie1
+{
+    class Class1 : ImaszynaLosujaca
+    {
+        private List<string> kupony = new();
+        private Random random = new();
+
+        public void dodajNowyKupon(string value)
+        {
+            kupony.Add(value);
+        }
+
+        public List<string> wszystkieKupony() 
+        {
+            return kupony;
+        }
+
+        public string wylowsowanyKupon()
+        {
+            int numerKuponu = random.Next(kupony.Count);
+            string wylosowanyKupon = kupony[numerKuponu];
+            kupony.RemoveAt(numerKuponu);
+            return wylosowanyKupon;
+        }
+
+        public bool listaKuponowPusta()
+        {
+            return !(kupony?.Any() ?? false);
+        }
+    }
+}
---
+++ Zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImaszynaLosujaca maszynaLosujaca;
+        public MainWindow()
+        {
+            InitializeComponent();
+            maszynaLosujaca = new Class1();
+        }
+
+        private void dodajKupon(object sender, RoutedEventArgs e)
+        {
+            if (textBoxItem.Text != string.Empty)
+            {
+                maszynaLosujaca.dodajNowyKupon(textBoxItem.Text);
+                textBoxItem.Text = "";
+                listaOdswierz();
+            }
+        }
+
+        private void wylosujKupon(object sender, RoutedEventArgs e)
+        {
+            if (!maszynaLosujaca.listaKuponowPusta())
+            {
+                string kupon = maszynaLosujaca.wylowsowanyKupon();
+                wylosowany.Content = kupon;
+                listaOdswierz();
+            }
+        }
+
+        private void listaOdswierz()
+        {
+            listaK.Items.Clear();
+            maszynaLosujaca.wszystkieKupony().ForEach(x => listaK.Items.Add(x));
+        }
+    }
+}
+
